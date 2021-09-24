de90115554ce0af7a92c0272b69200cdbc6daa31
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-09-24 08:16:22 -0500

    mensaje de saludo.domino

--- HolaWeb.App.Dominio/Saludo.cs
+++ HolaWeb.App.Dominio/Saludo.cs
@@ -4,6 +4,7 @@ namespace HolaWeb.App.Dominio
 {
     public class Saludo
     {
+        //la primary key se identifica con Id 
         public int Id {get;set;}
         public string EnEspañol {get;set;}
         public string EnIngles {get;set;}
